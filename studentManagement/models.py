866e30cd7a17dab305a5ac40286e4dab4ab5b475
Seu Nome <voce@exemplo.com>
2023-11-14 11:20:34 -0300

    atualização

--- studentManagement/models.py
+++ studentManagement/models.py
@@ -7,3 +7,7 @@ class Student(models.Model):
     Classe responsável pelo cadastro de alunos
     """
     name = models.CharField(max_length=50, blank=False)
+    matrícula = models.CharField(max_length=50, blank=False, default="0")
+    turma = models.CharField(max_length=20 , blank=False, default= "0")
+    idade = models.CharField(max_length=2, blank=False, default="0")
+    email = models.EmailField(max_length=50, blank=False, default="gmail")
